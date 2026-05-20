7835475e61bfe54684bd9c6ddcad1e1f73b57ed4
Muhammad Talha Shafiq <araintalha0304@gmail.com>
2026-05-20 21:10:53 +0500

    Updated the Auth Module and fixed bugs

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -13,9 +13,12 @@ router.post("/signup", validate(signUpSchema), signUp);
 router.post("/signin", validate(signInSchema), signIn);
 router.post("/refresh-token", refreshAccessToken);
 router.get("/me", protect, getMe);
+router.post("/logout", protect, logout);
+router.put("/change-password", protect, validate(changePasswordSchema), changePassword);
+router.put("/update-profile", protect, validate(updateProfileSchema), updateProfile);
 
 // Google OAuth routes
 router.get("/google", passport.authenticate("google", {scope: ["profile", "email"]}));
-router.get("/google/callback", passport.authenticate("google", { session: false }), googleSuccess);
+router.get("/google/callback", passport.authenticate("google", { session: false, failureRedirect: `${process.env.FRONTEND_URL}/login?error=oauth_failed`, }), googleSuccess);
 
 export default router;
--- backend/src/validations/auth.validation.js
+++ backend/src/validations/auth.validation.js
@@ -21,7 +21,31 @@ const signInSchema = z.object({
   }),
 });
 
+const updateProfileSchema = z.object({
+  body: z.object({
+    name: z.string().min(2, "Name must be at least 2 characters").optional(),
+    email: z.string().email("Invalid email").optional(),
+  }).refine((data) => data.name || data.email, {
+    message: "At least one field (name or email) is required",
+  }),
+});
+
+const changePasswordSchema = z.object({
+  body: z.object({
+    currentPassword: z.string().min(6, "Current password is required"),
+    newPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/,
+        "Password too weak: Needs upper, lower, number, and special character"
+      ),
+  })
+});
+
 export {
     signUpSchema,
     signInSchema,
+    changePasswordSchema,
+    updateProfileSchema,
 }
