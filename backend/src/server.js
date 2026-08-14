7a307fef62326c6aa6d40d05557c587a39e6607f
Muhammad Talha Shafiq <araintalha0304@gmail.com>
2026-08-14 16:03:57 +0500

    Fixed cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV !== 'test') {
 
 // middlewares
 app.use(cors({ 
-  origin: "http://localhost:5173", 
+  origin: process.env.FRONTEND_URL, 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
